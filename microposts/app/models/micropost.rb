437e85068687b1878d345a03a2e7b78cc5acc6f3
momoppii <81923879+momoppii@users.noreply.github.com>
2021-07-18 02:51:27 +0900

    Delete microposts directory

--- microposts/app/models/micropost.rb
+++
@@ -1,8 +0,0 @@
-class Micropost < ApplicationRecord
-  belongs_to :user
-  
-  has_many :favorites
-  has_many :favorited_users, through: :favorites, source: :micropost
-
-  validates :content, presence: true, length: { maximum: 255 }
-end
