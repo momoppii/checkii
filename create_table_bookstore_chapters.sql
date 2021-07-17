afff1fc1043100ca733481ef0e1d9d2290142915
momoppii <81923879+momoppii@users.noreply.github.com>
2021-07-18 02:54:29 +0900

    Delete create_table_bookstore_chapters.sql

--- create_table_bookstore_chapters.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE chapters (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    chapter_num INT,
-    title VARCHAR(100),
-    book_id INT,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
